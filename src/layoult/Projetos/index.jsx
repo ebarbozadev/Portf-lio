bdf83bdbd69df8c266a24e100f4761b733897194
ebarbozadev <e.barboza@edu.unipar.br>
2024-06-09 23:44:42 -0300

    Adicionando projeto movimento de estoque

--- src/layoult/Projetos/index.jsx
+++ src/layoult/Projetos/index.jsx
@@ -2,11 +2,24 @@ import { useState } from "react";
 import Video from "../Video";
 
 export default function Projetos() {
+    const [mvEstoque, setMvEstoque] = useState(false);
+    const [cadProduto, setCadProduto] = useState(false);
 
-    const [aberto, setAberto] = useState(false);
+    function video(e) {
+        const id = e.target.id;
 
-    function video() {
-        setAberto(!aberto);
+        if (id === 'cadProduto') {
+            setCadProduto(true);
+            setMvEstoque(false);
+        } else if (id === 'mvEstoque') {
+            setCadProduto(false);
+            setMvEstoque(true);
+        }
+    }
+
+    function fecharVideo() {
+        setCadProduto(false);
+        setMvEstoque(false);
     }
 
     return (
@@ -18,7 +31,7 @@ export default function Projetos() {
                         <h4>Cadastro de Produto</h4>
 
                         <p>
-                            Poderá cadastrar seus produtos visualizando o estoque atual, e também ter controle sobre o custo de venda e custo de venda
+                            Poderá cadastrar seus produtos visualizando o estoque atual, e também ter controle sobre o custo de venda e custo de venda.
                         </p>
 
                         <div className="tecnologias">
@@ -29,7 +42,26 @@ export default function Projetos() {
 
                         <div className="card-botao">
                             <button onClick={() => alert('Entre em contato')}>Código</button>
-                            <button onClick={() => video()}>Vídeo</button>
+                            <button id="cadProduto" onClick={(e) => video(e)}>Vídeo</button>
+                        </div>
+                    </div>
+
+                    <div className="projeto-card">
+                        <h4>Movimento de Estoque</h4>
+
+                        <p>
+                            Tenha total controle sobre seu estoque ficando livre para dar entrada ou saída de estoque.
+                        </p>
+
+                        <div className="tecnologias">
+                            <p className="linguagem">JavaScript</p>
+                            <p className="linguagem">NodeJS</p>
+                            <p className="linguagem">ReactJS</p>
+                        </div>
+
+                        <div className="card-botao">
+                            <button onClick={() => alert('Entre em contato')}>Código</button>
+                            <button id="mvEstoque" onClick={(e) => video(e)}>Vídeo</button>
                         </div>
                     </div>
 
@@ -45,13 +77,10 @@ export default function Projetos() {
                         <h4>Em breve...</h4>
                     </div>
 
-                    <div className="projeto-card">
-                        <h4>Em breve...</h4>
-                    </div>
-
-                    <Video aberto={aberto} fechar={() => video()} link={'https://www.youtube.com/embed/B3vot3THdMQ?si=Ds-C3_0km_pJKQR5'} />
+                    <Video aberto={cadProduto} fechar={fecharVideo} link={'https://www.youtube.com/embed/B3vot3THdMQ?si=Ds-C3_0km_pJKQR5'} />
+                    <Video aberto={mvEstoque} fechar={fecharVideo} link={'https://www.youtube.com/embed/tV0RuFm_l44?si=ugqkLGR88WS-Q-zJ'} />
                 </div>
             </section>
         </section>
-    )
+    );
 }
--- src/layoult/Video/index.jsx
+++ src/layoult/Video/index.jsx
@@ -3,14 +3,24 @@ export default function Video({ link, fechar, aberto }) {
         return (
             <section className="video">
                 <div className="container">
-                    <iframe className="visualizacao" width="560" height="315" src={link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <iframe
+                        className="visualizacao"
+                        width="560"
+                        height="315"
+                        src={link}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                    ></iframe>
 
                     <div className='botao'>
                         <button onClick={fechar}>x</button>
                     </div>
                 </div>
             </section>
-        )
+        );
     } else {
         return null;
     }
